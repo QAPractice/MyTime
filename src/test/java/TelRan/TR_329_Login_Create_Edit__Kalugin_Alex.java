105d99881e68624bab3f0e2e4b2bed91d50af0e7
AlexKlogin <iserjude@yahoo.com>
2015-04-03 21:36:17 +0300

    Test Run 329

--- src/test/java/TelRan/TR_329_Login_Create_Edit__Kalugin_Alex.java
+++ src/test/java/TelRan/TR_329_Login_Create_Edit__Kalugin_Alex.java
@@ -25,7 +25,6 @@ public class TR_329_Login_Create_Edit__Kalugin_Alex {
     private StringBuffer verificationErrors = new StringBuffer();
     private String Name ;
 
-
     @BeforeTest
     public void setUp() throws Exception {
         driver = new FirefoxDriver();
