30ab72f75dee04e00ac78e4cce8b0022e44dff9f
Iakov Volf <jakoff@gmail.com>
2015-04-18 22:12:53 +0300

    main page renamed

--- src/test/java/TelRan/pages/MainPage.java
+++ src/test/java/TelRan/pages/MainPage.java
@@ -1,6 +1,5 @@
 package TelRan.pages;
 
-import com.gargoylesoftware.htmlunit.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -9,7 +8,7 @@ import org.openqa.selenium.support.PageFactory;
 /**
  * Created by Anton on 18-Apr-15.
  */
-public class SharePage extends Page {
+public class MainPage extends Page {
 
     //buttons
     @FindBy(id = "'852'/td[2]" )
@@ -20,7 +19,7 @@ public class SharePage extends Page {
     WebElement ShareOnFacebookButton;
 //save
 
-    public SharePage(WebDriver driver) {
+    public MainPage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
     }
