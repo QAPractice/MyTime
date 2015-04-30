d498b177ae1ef75b0a6d561f2d37cd11a0c35993
Marina <stritenko@mail.ru>
2015-04-30 19:10:31 +0300

    Merge branch 'master' of C:\Users\Marina\IdeaProjects\MyTime with conflicts.

--- src/test/java/TelRan/pages/MainPage.java
+++ src/test/java/TelRan/pages/MainPage.java
@@ -6,38 +6,25 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 /**
- * Created by Anton on 18-Apr-15, Iakov Volf,
+ * Created by Anton on 18-Apr-15.
  */
 public class MainPage extends Page {
 
     //buttons Sharing
     @FindBy(xpath = "//*[@id='placetable']//tr[1]/td[contains(text(),'MyCalendar')]/../../tr[3]/td[3]/a")
-    WebElement shareButton;
+    WebElement ShareButton;
     @FindBy(xpath = "//*[@id='placetable']//tr[1]/td[contains(text(),'MyCalendar')]/../../tr[4]/td[2]/input")
-    WebElement shareMailButton;
+    WebElement ShareMailButton;
     @FindBy(xpath = "//*[@id='placetable']//tr[1]/td[contains(text(),'MyCalendar')]/../../tr[4]/td[3]/a")
-    WebElement shareOnGoogleButton;
+    WebElement ShareOnGoogleButton;
     @FindBy(xpath = "//*[@id='placetable']//tr[1]/td[contains(text(),'MyCalendar')]/../../tr[4]/td[4]/a")
-    WebElement shareOnFacebookButton;
-    @FindBy(xpath = "//*[@id='form2']/div/button")
-    WebElement linkToNewCalendar;
+    WebElement ShareOnFacebookButton;
+
 
     public MainPage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
     }
 
-    public void openNewCalendarPage() {
-        clickElement(linkToNewCalendar);
-
-        CreateCalendarOlgaPage createCalendarOlgaPage;
-        createCalendarOlgaPage = PageFactory.initElements(driver, CreateCalendarOlgaPage.class);
-    }
-
-    public void clickToShareButton() {
-        clickElement(shareButton);
-    }
-
 
 }
- 
