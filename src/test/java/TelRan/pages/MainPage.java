45115f87aa92c92df281ac53d290077a04d7d895
Iakov Volf <jakoff@gmail.com>
2015-05-04 18:41:50 +0300

    Retreiving methods of main page

--- src/test/java/TelRan/pages/MainPage.java
+++ src/test/java/TelRan/pages/MainPage.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.support.PageFactory;
 public class MainPage extends Page {
 
     //buttons Sharing
+
     @FindBy(xpath = "//*[@id='placetable']//tr[1]/td[contains(text(),'CalendarFirst')]/../../tr[3]/td[3]/a")
     WebElement calendarFirstshareButton;
     @FindBy(xpath = "//*[@id='placetable']//tr[1]/td[contains(text(),'CalendarFirst')]/../../tr[4]/td[2]/input")
@@ -20,17 +21,35 @@ public class MainPage extends Page {
     @FindBy(xpath = "//*[@id='placetable']//tr[1]/td[contains(text(),'CalendarFirst')]/../../tr[4]/td[4]/a")
     WebElement calendarFirstshareOnFacebookButton;
 
+    @FindBy(xpath = "//*[@id='placetable']//tr[3]/td[contains(text(),'CalendarFirst')]/input")  // REMOVE
+            WebElement calendarFirstRemoveButton;
+
     @FindBy(xpath = "//*[@id='placetable']//tr[1]/td[contains(text(),'4486432')]/../../tr[3]/td[3]/a")
     WebElement digitshareButton;
-    @FindBy(xpath = "//*[@id='placetable']//tr[1]/td[contains(text(),'MyCalendar')]/../../tr[4]/td[2]/input")
+    @FindBy(xpath = "//*[@id='placetable']//tr[1]/td[contains(text(),'4486432')]/../../tr[4]/td[2]/input")
     WebElement digitshareMailButton;
-    @FindBy(xpath = "//*[@id='placetable']//tr[1]/td[contains(text(),'MyCalendar')]/../../tr[4]/td[3]/a")
+    @FindBy(xpath = "//*[@id='placetable']//tr[1]/td[contains(text(),'4486432')]/../../tr[4]/td[3]/a")
     WebElement digitshareOnGoogleButton;
-    @FindBy(xpath = "//*[@id='placetable']//tr[1]/td[contains(text(),'MyCalendar')]/../../tr[4]/td[4]/a")
+    @FindBy(xpath = "//*[@id='placetable']//tr[1]/td[contains(text(),'4486432')]/../../tr[4]/td[4]/a")
     WebElement digitshareOnFacebookButton;
-    @FindBy(xpath = "//*[@id='form2']/div/button")
-    WebElement linkToNewCalendar;
 
+    @FindBy(xpath = "//*[@id='placetable']//tr[3]/td[contains(text(),'4486432')]/input")  // REMOVE
+            WebElement calendarDigitRemoveButton;
+
+    @FindBy(xpath = "//*[@id='placetable']//tr[1]/td[contains(text(),'4482Second')]/../../tr[3]/td[3]/a")
+    WebElement digitLettershareButton;
+    @FindBy(xpath = "//*[@id='placetable']//tr[1]/td[contains(text(),'4482Second')]/../../tr[4]/td[2]/input")
+    WebElement digitLettershareMailButton;
+    @FindBy(xpath = "//*[@id='placetable']//tr[1]/td[contains(text(),'4482Second')]/../../tr[4]/td[3]/a")
+    WebElement digitLettershareOnGoogleButton;
+    @FindBy(xpath = "//*[@id='placetable']//tr[1]/td[contains(text(),'4482Second')]/../../tr[4]/td[4]/a")
+    WebElement digitLettershareOnFacebookButton;
+
+    @FindBy(xpath = "//*[@id='placetable']//tr[3]/td[contains(text(),'4482Second')]/input")  // REMOVE
+            WebElement calendarSecondRemoveButton;
+
+    @FindBy(xpath = "//*[@id='form2']/div/button")   // CREATE A NEW AVAILABLE CALENDAR
+            WebElement linkToNewCalendar;
 
     public MainPage(WebDriver driver) {
         super(driver);
@@ -70,6 +89,47 @@ public class MainPage extends Page {
 
     }
 
+    public void clickToShareMailButtonSecondCal() {
+        clickElement(digitshareMailButton);
+        EmailAddressPage emailAddressPage;
+        emailAddressPage = PageFactory.initElements(driver, EmailAddressPage.class);
+    }
+
+    public void clickToShareOnGoogleButtonFirstCal() {
+        clickElement(calendarFirstshareOnGoogleButton);
+        SyncCooglePage syncCooglePage;
+        syncCooglePage = PageFactory.initElements(driver, SyncCooglePage.class);
+    }
+
+    public void clickToShareOnGoogleButtonDigitCal() {
+        clickElement(digitshareOnGoogleButton);
+        SyncCooglePage syncCooglePage;
+        syncCooglePage = PageFactory.initElements(driver, SyncCooglePage.class);
+    }
+
+    public void clickToShareOnGoogleButtonSecondCal() {
+        clickElement(digitLettershareOnGoogleButton);
+        SyncCooglePage syncCooglePage;
+        syncCooglePage = PageFactory.initElements(driver, SyncCooglePage.class);
+    }
+
+    public void clickToShareOnFacebookButtonFirstCal() {
+        clickElement(calendarFirstshareOnFacebookButton);
+        SyncFacebookPage syncFacebookPage;
+        syncFacebookPage = PageFactory.initElements(driver, SyncFacebookPage.class);
+    }
+
+    public void clickToShareOnFacebookButtonDigitCal() {
+        clickElement(digitshareOnFacebookButton);
+        SyncFacebookPage syncFacebookPage;
+        syncFacebookPage = PageFactory.initElements(driver, SyncFacebookPage.class);
+    }
+
+    public void clickToShareOnFacebookButtonSecondCal() {
+        clickElement(digitLettershareOnFacebookButton);
+        SyncFacebookPage syncFacebookPage;
+        syncFacebookPage = PageFactory.initElements(driver, SyncFacebookPage.class);
+    }
 
 }
  
