b34787b613a846397ccc56442591ef5a1c7d55ed
Iakov Volf <jakoff@gmail.com>
2015-05-07 19:04:49 +0300

    Added new methods in Page, Added new methods for color checking and data choosing

--- src/test/java/TelRan/EditCalendarTest.java
+++ src/test/java/TelRan/EditCalendarTest.java
@@ -1,50 +1,54 @@
 package TelRan;
 
+import TelRan.pages.EditCalendarPage;
 import TelRan.pages.LoginPage;
 import TelRan.pages.MainPage;
-import TelRan.pages.LoginPage;
-import TelRan.pages.EditCalendarPage;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.testng.AssertJUnit.assertFalse;
 import static org.testng.AssertJUnit.assertTrue;
 
 /**
- * Created by Iakov Volf on 5/4/2015.
+ * Created by Iakov Volf, Oleg on 5/4/2015.
  */
 public class EditCalendarTest {
 
     public WebDriver driver;
     public WebDriverWait wait;
-    private boolean acceptNextAlert = true;
     LoginPage loginPage;
-    private StringBuffer verificationErrors = new StringBuffer();
     EditCalendarPage editCalendarPage;
     MainPage mainPage;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
 
-    @BeforeMethod(alwaysRun = true)
+    @BeforeClass(alwaysRun = true)
     public void setup() {
         this.driver = new FirefoxDriver();
         wait = new WebDriverWait(driver, 5);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         loginPage = PageFactory.initElements(driver, LoginPage.class);
-        loginPage.openLoginPage();
         mainPage = PageFactory.initElements(driver, MainPage.class);
+        loginPage.openLoginPage();
+        try {
+            loginPage.login("telrantests@yahoo.com", "12345.com");
+            mainPage.waitUntilMainPageIsLoaded();
+            mainPage.clickToEditButtonFirstCal();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void TestLoginSuccess() {
+    public void TestEditPageSuccess() {
         try {
-            loginPage.login("telrantests@yahoo.com", "12345.com");
             editCalendarPage.waitUntilEditPageIsLoaded();
             editCalendarPage.setStartDate("5","11");
             editCalendarPage.setEndDate("8","8");
